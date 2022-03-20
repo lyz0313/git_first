b30e584baaa3f06698415d0c18846c0595f053f1
lyz_win <446440497@qq.com>
2022-03-21 00:48:39 +0800

    node.js study 2

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-var http = require("http")
+var http = require('http');
+var url = require('url');
+var util = require('util');
 
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+
+    var params = url.parse(request.url, true).query;
+    /*response.write("net name: " + params.name);
+    response.write("\n");
+    response.write("net URL: " + params.url);*/
+    response.end(util.inspect(url.parse(request.url, true)));
 }).listen(8888);
 
 console.log('Server running at http://127.0.0.1:8888/');
